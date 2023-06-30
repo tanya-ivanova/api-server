ef92f973cf2e53a2faca785192e7ffc7b1acbd6b
tanya-ivanova <ivanova.itanya@gmail.com>
2023-06-30 21:36:02 +0300

    Encoding the search value

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -4,11 +4,12 @@ const {searchPosts} = require('../services/searchService');
 const searchController = require('express').Router();
 
 searchController.get('/', async (req, res) => {   
-    const search = req.query.search;      
+    const search = req.query.search; 
+    const decodedSearch = decodeURIComponent(search);     
 
     let searchResults = [];
     if(search) {
-        searchResults = await searchPosts(search);
+        searchResults = await searchPosts(decodedSearch);
     }
     
     res.json(searchResults);   
