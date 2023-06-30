678e236f5d5afd8ef352c0a5bccb203818191591
tanya-ivanova <ivanova.itanya@gmail.com>
2023-06-30 17:32:16 +0300

    Installing node-fetch

--- services/weatherService.js
+++ services/weatherService.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 async function getWeather() {
     const res = await fetch('http://api.weatherapi.com/v1/forecast.json?key=795b320e04e64bbb9aa42005232906&q=46.43,11.85&days=3');
 
