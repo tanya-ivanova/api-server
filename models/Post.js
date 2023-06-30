dcd024161a6df3f197c1489b9d72dd4b37f24d7a
tanya-ivanova <ivanova.itanya@gmail.com>
2023-06-30 18:26:41 +0300

    Updating the Post model

--- models/Post.js
+++ models/Post.js
@@ -5,6 +5,8 @@ const postSchema = new Schema({
     mountain: {type: String, required: true, minLength: [3, 'Mountain must be at least 3 characters long']},
     country: {type: String, required: true, minLength: [3, 'Country must be at least 3 characters long']},
     duration: {type: String, required: true, minLength: [2, 'Duration must be at least 2 characters long']},
+    latitude: {type: Number, required: true},
+    longitude: {type: Number, required: true},
     description: {type: String, required: true, minLength: [10, 'Description must be at least 10 characters long']},
     photos: {type: [String], required: [true, 'At least one photo is required']},    
     //_ownerId: {type: ObjectId, ref: 'User', required: true}
