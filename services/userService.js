050be5ff0685c26e717315548816a5cb4c7be1d1
tanya-ivanova <ivanova.itanya@gmail.com>
2023-07-14 20:35:32 +0300

    Removing token expiration

--- services/userService.js
+++ services/userService.js
@@ -48,7 +48,7 @@ function createToken(user) {
     return {
         _id: user._id,
         email: user.email,
-        accessToken: jwt.sign(payload, secret, {expiresIn: 3000})
+        accessToken: jwt.sign(payload, secret)
     };
 }
 
