c641ba7258842985bea36a8b7701f8c8f4c309df
tanya-ivanova <ivanova.itanya@gmail.com>
2023-07-18 07:36:03 +0300

    Updating search service

--- services/searchService.js
+++ services/searchService.js
@@ -3,7 +3,7 @@ const Post = require('../models/Post');
 async function searchPosts(search) {  
     return Post.find({
         name: {$regex: search, $options: 'i'},
-    });
+    }).populate('_ownerId', 'email _id');
 }
 
 module.exports = {    
