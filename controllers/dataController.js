4ca6ef4e0bf1128d0c1b90cd893e97e5ab723f11
tanya-ivanova <ivanova.itanya@gmail.com>
2023-07-23 10:54:00 +0300

    Updating error handling

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,29 +1,29 @@
 const { getAll, create, getById, update, deleteById, getByUserId, postLike, postComment } = require('../services/postService');
 const { hasUser } = require('../middlewares/guards');
-const {parseError} = require('../util/parser');
+const { parseError } = require('../util/parser');
 
 const dataController = require('express').Router();
 
 dataController.get('/', async (req, res) => {
     let items = [];
-    if(req.query.where) {
+    if (req.query.where) {
         const userId = JSON.parse(req.query.where.split('=')[1]);
         items = await getByUserId(userId);
     } else {
         items = await getAll();
     }
-    res.json(items);    
+    res.json(items);
 });
 
 dataController.post('/', hasUser(), async (req, res) => {
     try {
-        const data = Object.assign({_ownerId: req.user._id}, req.body);
+        const data = Object.assign({ _ownerId: req.user._id }, req.body);
         const item = await create(data);
         res.json(item);
 
     } catch (error) {
         const message = parseError(error);
-        res.status(400).json({message});
+        res.status(400).json({ message });
     }
 });
 
@@ -34,8 +34,8 @@ dataController.get('/:id', async (req, res) => {
 
 dataController.put('/:id', hasUser(), async (req, res) => {
     const item = await getById(req.params.id);
-    if(req.user._id != item._ownerId._id) {
-        return res.status(403).json({message: 'You can\'t modify this record'});
+    if (req.user._id != item._ownerId._id) {
+        return res.status(403).json({ message: 'You can\'t modify this record' });
     }
 
     try {
@@ -44,14 +44,14 @@ dataController.put('/:id', hasUser(), async (req, res) => {
 
     } catch (error) {
         const message = parseError(error);
-        res.status(400).json({message});
+        res.status(400).json({ message });
     }
 });
 
 dataController.delete('/:id', hasUser(), async (req, res) => {
     const item = await getById(req.params.id);
-    if(req.user._id != item._ownerId._id) {
-        return res.status(403).json({message: 'You can\'t modify this record'});
+    if (req.user._id != item._ownerId._id) {
+        return res.status(403).json({ message: 'You can\'t modify this record' });
     }
 
     try {
@@ -60,18 +60,29 @@ dataController.delete('/:id', hasUser(), async (req, res) => {
 
     } catch (error) {
         const message = parseError(error);
-        res.status(400).json({message});
+        res.status(400).json({ message });
     }
 });
 
-dataController.post('/:id/likes', async (req, res) => {    
+dataController.post('/:id/likes', hasUser(), async (req, res) => {
+    const item = await getById(req.params.id);
+    if (req.user._id == item._ownerId._id) {
+        return res.status(403).json({ message: 'You can\'t like your own post' });
+    }
+
     await postLike(req.params.id, req.user._id);
     res.status(200).json({ message: 'Liked successful!' });
 });
 
-dataController.post('/:id/comments', async (req, res) => {     
-    const comment = await postComment(req.body.content, req.params.id, req.user._id);
-    res.json(comment);
+dataController.post('/:id/comments', hasUser(), async (req, res) => {
+    try {
+        const comment = await postComment(req.body.content, req.params.id, req.user._id);
+        res.json(comment);
+
+    } catch (error) {
+        const message = parseError(error);
+        res.status(400).json({ message });
+    }
 });
 
 module.exports = dataController;
