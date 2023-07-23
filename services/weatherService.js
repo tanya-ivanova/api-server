9e901ee5c5733352de619ba12d113c80f2b136a4
tanya-ivanova <ivanova.itanya@gmail.com>
2023-07-23 07:14:59 +0300

    Updating error handling

--- services/weatherService.js
+++ services/weatherService.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 async function getWeather(latitude, longitude) {
-    const res = await fetch(`http://api.weatherapi.com/v1/forecast.json?q=${latitude},${longitude}&days=3`);
+    const res = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=795b320e04e64bbb9aa42005232906&q=${latitude},${longitude}&days=3`);
 
     const result = await res.json();    
     return result;    
